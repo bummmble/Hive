532bc5ca9ec08345ac3f1e1a0380b11ccbe335fa
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-12 00:43:07 -0600

    Adds some security middleware

---
+++ src/middleware/security.js
@@ -0,0 +1,27 @@
+import helmet from 'helmet';
+import parameterProtection from 'hpp';
+import uuid from 'uuid';
+
+export default function createSecurity(server, { enableNonce = true }) {
+  if (enableNonce) {
+    server.use((req, res, next) => {
+      res.locals.nonce = uuid();
+      next();
+    });
+  }
+
+  // Don't expose software information
+  server.disable('x-powered-by');
+
+  // Prevent HTTP Parameter pollution
+  server.use(parameterProtection());
+
+  // Sets X-XSS-Protection header to prevent reflected XSS attacks
+  server.use(helmet.xssFilter());
+
+  // Mitigates clickjacking attacks by setting the X-Frame-Options header
+  server.use(helmet.frameguard('deny'));
+
+  server.use(helmet.ieNoOpen());
+  server.use(helmet.noSniff());
+}
