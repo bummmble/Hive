c8762e4e2c565f830ce7c98cccd23c24ff1bf43e
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-12 00:48:39 -0600

    Exposes create server function

---
+++ src/create.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import createCore from './middleware/core';
+import createError from './middleware/error';
+import createFallback from './middleware/fallback';
+import createSecurity from './middleware/security';
+
+const defaultStatic = {
+  public: '/static',
+  path: 'build/client',
+};
+
+export default function createServer({
+  staticConfig = defaultStatic,
+  afterSecurity = [],
+  beforeFallback = [],
+  enableNonce = false,
+}) {
+  const server = express();
+  createError(server);
+  createSecurity(server, { enableNonce });
+
+  if (afterSecurity.length > 0) {
+    afterSecurity.forEach((middleware) => {
+      if (Array.isArray(middleware)) {
+        server.use(...middleware);
+      } else {
+        server.use(middleware);
+      }
+    });
+  }
+
+  createCore(server);
+
+  if (staticConfig) {
+    server.use(staticConfig.public, express.static(staticConfig.path));
+  }
+
+  if (beforeFallback.length > 0) {
+    afterSecurity.forEach((middleware) => {
+      if (Array.isArray(middleware)) {
+        server.use(...middleware);
+      } else {
+        server.use(middleware);
+      }
+    });
+  }
+
+  createFallback(server);
+
+  return server;
+}
